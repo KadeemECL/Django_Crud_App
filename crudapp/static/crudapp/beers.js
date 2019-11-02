b032c95713690fa52a5fe858f36a7d14cf2d47d8
Kadeem Clarke Legg <Kadeemcl@gmail.com>
2019-11-01 23:31:27 -0400

    Added random brewery feature to home page. Adjusted navbar style

--- crudapp/static/crudapp/beers.js
+++ crudapp/static/crudapp/beers.js
@@ -1,19 +1,29 @@
 const request = new XMLHttpRequest();
 
 const cors = 'https://cors-anywhere.herokuapp.com/';
-const beerAPI = 'https://sandbox-api.brewerydb.com/v2/breweries/?key=f369558d6aa24a045494dc446733fd8c'
+const beerAPI = 'https://sandbox-api.brewerydb.com/v2/breweries/?key=f369558d6aa24a045494dc446733fd8c';
+
+var counter = 0;
 
 request.open('GET', cors + beerAPI, true);
 
+
 request.onload = function () {
+
+
     const data = JSON.parse(this.response);
 
     if(request.status >= 200 && request.status < 400){
         data.data.forEach(beer => {
             const beerSite = beer.website;
             const beerName = beer.name;
-            console.log(beerSite);
-            console.log(beerName);
+
+            /*Beer Page*/
+            counter+=1;
+
+            console.log(beerName + " " + counter);
+            console.log(beerSite + " " + counter);
+
 
             const node = document.createElement('a');
             node.setAttribute('href', beerSite);
@@ -21,8 +31,18 @@ request.onload = function () {
             node.setAttribute('class', 'yo');
             const textnode = document.createTextNode(beerName);
             node.appendChild(textnode);
-            document.getElementById('beerList').appendChild(node);
 
+            if(counter <= 6){
+                document.getElementById('beerList').appendChild(node);
+            } else if(counter >= 7 && counter <= 12){
+                document.getElementById('beerList1').appendChild(node);
+            } else {
+                document.getElementById('beerList2').appendChild(node);
+            }
+
+            if(counter >= 18){
+                request.send(false)
+            }
 
         })
     } else {
@@ -31,3 +51,5 @@ request.onload = function () {
 };
 
 request.send();
+
+
